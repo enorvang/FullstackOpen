c8d44d7948517d93a2dd37d2512a1c767f24a6e2
enorvang <espen.norvang@gmail.com>
2020-02-29 00:16:41 +0100

    started on unicafe

---
+++ part1/unicafe/unicafe/src/index.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const App = (props) => {
+    const [ counter, setCounter ] = useState(0)
+
+    const setToValue = (value) => setCounter(value)
+
+    return (
+      <div>
+        <div>{counter}</div>
+        <button onClick={setToValue(counter + 1)}>
+                plus      
+        </button> 
+        <button onClick={setToValue(0)}>
+            zero
+        </button>
+    </div>
+    )
+  }
+
+  ReactDOM.render(<App />, document.getElementById('root'))
