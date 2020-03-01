643e791cb980256bc38237a40af32c6d83f66b33
enorvang <espen.norvang@gmail.com>
2020-03-01 13:54:05 +0100

    finished task 1.11

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -3,35 +3,22 @@ import ReactDOM from 'react-dom'
 
 const Button = ( {handleClick, text }) => <button onClick={handleClick}>{text}</button>
 const Header = ( {text} ) => <h1>{text}</h1>
+
 const Statistics = (props) => {
-  let good = props.good
-  let neutral = props.neutral
-  let bad = props.bad
-  let total = good + neutral + bad
-  let average = (good + (neutral * 0) + (bad * -1) ) / total
-  let percentagePositive = (good / total) * 100
-  if(!props.hasFeedback){
-    return (
-      <div>
-        No feedback given
-      </div>
-    )
-  }else {
-    return (
-      <div>
-        good {good} <br/>
-        neutral {neutral} <br/>
-        bad {bad} <br/>
-        total {total} <br/>
-        average {average} <br/>
-        positive {percentagePositive} %<br/>
-      </div>
-    )
+  let value = props.value
+  if(isNaN(value)){
+    value = 0
   }
+  return (
+          <tr>
+            <td>{props.text}</td> 
+            <td>{value}</td> 
+            <td>{props.percentage}</td>
+          </tr>
+  )
 }
 
 const App = () => {
-  // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
@@ -65,8 +52,16 @@ const App = () => {
       <Button handleClick={handleNeutralClick} text='neutral'/>
       <Button handleClick={handleBadClick} text='bad'/>
       <Header text={statsText}/>
-      <Statistics hasFeedback={hasFeedback} good={good} neutral={neutral} bad={bad}/>
-      
+      <table>
+        <tbody>
+      <Statistics text="good" value={good} />
+      <Statistics text="neutral" value={neutral} />
+      <Statistics text="bad" value={bad} />
+      <Statistics text="total" value={total} />
+      <Statistics text="average" value={average} />
+      <Statistics text="positive" value={percentagePositive} />
+        </tbody>
+      </table>
     </div>
   )
 }
