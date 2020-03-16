e66f8b3e39e9f118b4e5c6f071deded168ca49ea
enorvang <espen.norvang@gmail.com>
2020-03-16 14:23:33 +0100

    changed variable names

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,71 +1,71 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import peopleService from "./services/peopleServices"
 import People from './components/People'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
 
 
 const App = () => {
-  const [ persons, setPersons] = useState([]) 
-  const [ newName, setNewName ] = useState('')
-  const [ newNumber, setNewNumber ] = useState('')
-  const [ filter, setFilter ] = useState('')
+    const [people, setPeople] = useState([])
+    const [newName, setNewName] = useState('')
+    const [newNumber, setNewNumber] = useState('')
+    const [filter, setFilter] = useState('')
 
-  useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
-      })
-  }, [])
+    useEffect(() => {
+        peopleService
+            .getAll()
+            .then(initialPeople => {
+                setPeople(initialPeople)
+            })
+    }, [])
 
-  const handleFilterChange = (event) => {
-    setFilter(event.target.value)
-  }
-
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
-
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
-
-  const displayPeople = (filter.length === 0)
-  ? persons
-  : persons.filter(p => p.name.toLowerCase().includes(filter.toLowerCase()))
-
-
-  const addNewEntry = (event) => {
-    event.preventDefault()
-    const personObject = {
-      name: newName,
-      number: newNumber
+    const handleFilterChange = (event) => {
+        setFilter(event.target.value)
     }
 
-    if(persons.some(p => p.name === newName)){
-      alert(`${newNumber} already exists`)
-    }else {
-      setPersons(persons.concat(personObject))
-      setNewName('')
-      setNewNumber('')
+    const handleNameChange = (event) => {
+        setNewName(event.target.value)
     }
-  }
-  
 
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <Filter filter={handleFilterChange}/>
+    const handleNumberChange = (event) => {
+        setNewNumber(event.target.value)
+    }
 
-      <h3>Add new</h3>
-      <PersonForm addNewEntry={addNewEntry} newName={newName} handleNameChange={handleNameChange} 
-        newNumber={newNumber} handleNumberChange={handleNumberChange}/>
-       
-      <h2>Numbers</h2>
-      <People people={displayPeople} />
-    </div>
-  )
+    const displayPeople = (filter.length === 0)
+        ? people
+        : people.filter(p => p.name.toLowerCase().includes(filter.toLowerCase()))
+
+
+    const addNewEntry = (event) => {
+        event.preventDefault()
+        const personObject = {
+            name: newName,
+            number: newNumber
+        }
+
+        if (people.some(p => p.name === newName)) {
+            alert(`${newNumber} already exists`)
+        } else {
+            setPeople(people.concat(personObject))
+            setNewName('')
+            setNewNumber('')
+        }
+    }
+
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <Filter filter={handleFilterChange} />
+
+            <h3>Add new</h3>
+            <PersonForm addNewEntry={addNewEntry} newName={newName} handleNameChange={handleNameChange}
+                newNumber={newNumber} handleNumberChange={handleNumberChange} />
+
+            <h2>Numbers</h2>
+            <People people={displayPeople} />
+        </div>
+    )
 }
 
 export default App
