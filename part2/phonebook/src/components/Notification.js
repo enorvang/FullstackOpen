fd306d6e74df3b2c9ba275b4e57f8518b3ad64b6
enorvang <espen.norvang@gmail.com>
2020-03-16 20:31:00 +0100

    separate colors on errors

--- part2/phonebook/src/components/Notification.js
+++ part2/phonebook/src/components/Notification.js
@@ -1,15 +1,22 @@
 import React from 'react'
 
 const Notification = ({ message }) => {
-  if (message === null){
+  if (message === null) {
     return null
-  }
+  } else if (message.charAt.toLowerCase() ==='d') {
+    return (
+      <div className='error'>
+        {message}
+      </div>
+    )
+  } else {
 
-  return (
-    <div className='notification'> 
-      {message}
-    </div>
-  )
+    return (
+      <div className='notification'>
+        {message}
+      </div>
+    )
+  }
 }
 
 export default Notification
