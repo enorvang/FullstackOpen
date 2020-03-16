8fc77c5c63e162cb7cc370aaf923018e9facef0e
enorvang <espen.norvang@gmail.com>
2020-03-16 15:50:40 +0100

    fixed indents

--- part2/phonebook/src/components/PersonForm.js
+++ part2/phonebook/src/components/PersonForm.js
@@ -2,18 +2,18 @@ import React from 'react'
 
 const PersonForm = ({ addNewEntry, newName, handleNameChange, newNumber, handleNumberChange }) => {
     return (
-      <form onSubmit={addNewEntry}>
-        <div>
-        name: <input value={newName} onChange={handleNameChange}/>
-        </div>
-        <div>
-        number: <input value={newNumber} onChange={handleNumberChange}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+        <form onSubmit={addNewEntry}>
+            <div>
+                name: <input value={newName} onChange={handleNameChange} />
+            </div>
+            <div>
+                number: <input value={newNumber} onChange={handleNumberChange} />
+            </div>
+            <div>
+                <button type="submit">add</button>
+            </div>
+        </form>
     )
-  }
+}
 
-  export default PersonForm
+export default PersonForm
