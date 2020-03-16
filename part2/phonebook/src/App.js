3142a1803a737f91a0716f6c9c4e11de81987647
enorvang <espen.norvang@gmail.com>
2020-03-16 16:31:45 +0100

    changed indents 4 -> 2 spaces

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,102 +6,102 @@ import peopleService from "./services/peopleServices"
 
 
 const App = () => {
-    const [people, setPeople] = useState([])
-    const [newName, setNewName] = useState("")
-    const [newNumber, setNewNumber] = useState("")
-    const [filter, setFilter] = useState("")
+  const [people, setPeople] = useState([])
+  const [newName, setNewName] = useState("")
+  const [newNumber, setNewNumber] = useState("")
+  const [filter, setFilter] = useState("")
 
-    useEffect(() => {
+  useEffect(() => {
+    peopleService
+      .getAll()
+      .then(initialPeople => {
+        setPeople(initialPeople)
+      })
+  }, [])
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const displayPeople = (filter.length === 0)
+    ? people
+    : people.filter(p => p.name.toLowerCase().includes(filter.toLowerCase()))
+
+
+  const addNewEntry = (event) => {
+    event.preventDefault()
+    const person = {
+      name: newName,
+      number: newNumber
+    }
+    if (people.some(p => p.number === newNumber)) {
+      alert(`${newNumber} already exists`)
+
+    } else if (people.some(p => p.name === newName)) {
+
+      if (window.confirm(`${newName} is already added to the phonebook. Do you want to replace replace the old phone number with a new one?`)) {
+        const personToUpdate = people.find(p => p.name === newName)
+        const id = personToUpdate.id
+        const changedPerson = { ...personToUpdate, number: newNumber }
+
         peopleService
-            .getAll()
-            .then(initialPeople => {
-                setPeople(initialPeople)
-            })
-    }, [])
+          .update(id, changedPerson)
+          .then(returnedPerson => {
+            setPeople(people.map(person => person.id !== id ? person : returnedPerson))
+          })
+      }
 
-    const handleFilterChange = (event) => {
-        setFilter(event.target.value)
+    } else {
+      peopleService
+        .create(person)
+        .then(returnedPerson => {
+          setPeople(people.concat(returnedPerson))
+          setNewName("")
+          setNewNumber("")
+        })
+        .catch(error => {
+          console.log(error.response)
+
+        })
     }
+  }
 
-    const handleNameChange = (event) => {
-        setNewName(event.target.value)
+  const deleteEntry = id => {
+    const personToDelete = people.find(p => p.id === id)
+    if (window.confirm(`Do you really want to delete ${personToDelete.name} ?`)) {
+      peopleService
+        .deleteEntry(id)
+        .then(returned => {
+          console.log(`Deleted ${personToDelete.name}`)
+        })
+        .catch(error => {
+          alert(`${personToDelete.name} was already deleted`)
+        })
+      setPeople(people.filter(p => p.id !== id))
     }
+  }
 
-    const handleNumberChange = (event) => {
-        setNewNumber(event.target.value)
-    }
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter filter={handleFilterChange} />
 
-    const displayPeople = (filter.length === 0)
-        ? people
-        : people.filter(p => p.name.toLowerCase().includes(filter.toLowerCase()))
+      <h3>Add new</h3>
+      <PersonForm addNewEntry={addNewEntry} newName={newName} handleNameChange={handleNameChange}
+        newNumber={newNumber} handleNumberChange={handleNumberChange} />
 
-
-    const addNewEntry = (event) => {
-        event.preventDefault()
-        const person = {
-            name: newName,
-            number: newNumber
-        }
-        if (people.some(p => p.number === newNumber)) {
-            alert(`${newNumber} already exists`)
-
-        } else if (people.some(p => p.name === newName)) {
-
-            if (window.confirm(`${newName} is already added to the phonebook. Do you want to replace replace the old phone number with a new one?`)) {
-                const personToUpdate = people.find(p => p.name === newName)
-                const id = personToUpdate.id
-                const changedPerson = { ...personToUpdate, number: newNumber }
-
-                peopleService
-                    .update(id, changedPerson)
-                    .then(returnedPerson => {
-                        setPeople(people.map(person => person.id !== id ? person : returnedPerson))
-                    })
-            }
-
-        } else {
-            peopleService
-                .create(person)
-                .then(returnedPerson => {
-                    setPeople(people.concat(returnedPerson))
-                    setNewName("")
-                    setNewNumber("")
-                })
-                .catch(error => {
-                    console.log(error.response)
-
-                })
-        }
-    }
-
-    const deleteEntry = id => {
-        const personToDelete = people.find(p => p.id === id)
-        if (window.confirm(`Do you really want to delete ${personToDelete.name} ?`)) {
-            peopleService
-                .deleteEntry(id)
-                .then(returned => {
-                    console.log(`Deleted ${personToDelete.name}`)
-                })
-                .catch(error => {
-                    alert(`${personToDelete.name} was already deleted`)
-                })
-            setPeople(people.filter(p => p.id !== id))
-        }
-    }
-
-    return (
-        <div>
-            <h2>Phonebook</h2>
-            <Filter filter={handleFilterChange} />
-
-            <h3>Add new</h3>
-            <PersonForm addNewEntry={addNewEntry} newName={newName} handleNameChange={handleNameChange}
-                newNumber={newNumber} handleNumberChange={handleNumberChange} />
-
-            <h2>Numbers</h2>
-            <People people={displayPeople} deleteEntry={deleteEntry} />
-        </div>
-    )
+      <h2>Numbers</h2>
+      <People people={displayPeople} deleteEntry={deleteEntry} />
+    </div>
+  )
 }
 
 export default App
--- part2/phonebook/src/components/Person.js
+++ part2/phonebook/src/components/Person.js
@@ -2,9 +2,9 @@ import React from "react"
 
 const Person = ({ person, deleteEntry }) => {
 
-    return (
-        <li>{person.name} {person.number} <button onClick={() => deleteEntry(person.id)}>delete</button> </li>
-    )
+  return (
+    <li>{person.name} {person.number} <button onClick={() => deleteEntry(person.id)}>delete</button> </li>
+  )
 }
 
 export default Person
