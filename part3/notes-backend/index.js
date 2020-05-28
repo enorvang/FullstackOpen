41ff94529113526a3359edc7e208d10aae23e45b
enorvang <espen.norvang@gmail.com>
2020-05-28 17:18:43 +0200

    started part3

---
+++ part3/notes-backend/index.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const app = express()
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy", 
+    date: "2019-05-30T17:30:31.098Z", 
+    important: true
+  },
+  {
+    id: 2, 
+    content: "Browser can execute only Javascript", 
+    date: "2019-05-30T18:39:34.091Z", 
+    important: false
+  },
+  {
+    id: 3, 
+    content: "GET and POST are the most important methods of HTTP protocol", 
+    date: "2019-05-30T19:20:14.298Z", 
+    important: true
+  },
+  {
+    id: 4,
+    content: "PUT and DELETE are not as important, but they have their use cases",
+    date: "2020-05-28T16:46:14.298Z"
+  }
+]
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes)
+})
+
+app.get('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const note = notes.find(note => note.id === id)
+  if(note){
+    res.json(note)
+  }else {
+    res.status(404).end()
+  }
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id)
+  notes = notes.filter(note => note.id !== id)
+  Response.status(204).end()
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
